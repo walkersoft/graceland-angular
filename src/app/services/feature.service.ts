10a834a105e506c193cce08dc03b0ebb89d51138
Jason L. Walker <jasonlanewalker@gmail.com>
2021-07-13 00:20:53 -0500

    quick and dirty implementation of feature store

---
+++ src/app/services/feature.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IFeature } from 'src/shared/interfaces/IFeature';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeatureService {
+  featureStore: string = 'assets/dummy-data/features.json';
+  constructor(private http: HttpClient) { }
+
+  getFeatures(): Observable<IFeature> {
+    return this.http.get<IFeature>(this.featureStore);
+  }
+}
